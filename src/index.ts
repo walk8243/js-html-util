597a5b303f285d317227b10e8188892b5bd1c8af
walk8243 <walk8243@gmail.com>
2022-05-04 22:15:06 +0900

    諸々調整してダウンロード関数を追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-const getElement = (selector: string, parent: HTMLElement | Document = document) => {
+export const getElement = (selector: string, parent: Element | Document = document) => {
 	return parent.querySelector(selector);
 };
-
-export {
-	getElement,
+export const getElementByClass = (className: string, parent: Element | Document = document) => {
+	const collections = parent.getElementsByClassName(className);
+	return collections.length > 0 ? collections[0] : null;
 };
+export const getElementByTag = <K extends keyof HTMLElementTagNameMap>(tag: K, parent: Element | Document = document) => {
+	const collections = parent.getElementsByTagName(tag);
+	return collections.length > 0 ? collections[0] : null;
+};
+
+export const downloadCanvas = (canvas: HTMLCanvasElement, filename: string, target?: HTMLAnchorElement) => {
+	const aTag = target || document.createElement('a');
+	aTag.download = filename + '.png';
+	aTag.href = canvas.toDataURL('image/png');
+	aTag.click();
+}
+
+const utils = {
+	getElement,
+	getElementByClass,
+	getElementByTag,
+
+	downloadCanvas,
+};
+
+export default utils;
